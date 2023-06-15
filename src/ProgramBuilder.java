f741dfd2b3d72061e27fa1d14fd4bd14bb0ba1bb
Wiktoria Siekierska <ws448477@students.mimuw.edu.pl>
2023-06-15 03:11:17 +0200

    Dodanie reszty builderów, aktualizacja pozostałych instrukcji z nowym środowiskiem zmiennych.

---
+++ src/ProgramBuilder.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class ProgramBuilder extends BlokBuilder{
+    private ArrayList<Blok> bloczki;
+    public ProgramBuilder(){
+        super();
+        this.bloczki=new ArrayList<>();
+    }
+    public void Blok(Blok d){
+        instrukcje.add(d);
+        bloczki.add(d);
+    }
+    public Program build(){
+        return new Program(instrukcje, bloczki, zmienne);
+    }
+}
