cafbe8ab592de22e7df5805490deb8c78615413d
Wiktoria Siekierska <ws448477@students.mimuw.edu.pl>
2023-06-15 02:00:39 +0200

    Zapomniałam o jednym pliku :00

---
+++ src/Zmienne.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+
+public class Zmienne {
+    public ArrayList<Zmienna> zmienne;
+    public Zmienne(ArrayList<Zmienna> zmienne) {
+        this.zmienne = zmienne;
+    }
+    public Zmienne() {
+        this.zmienne = new ArrayList<>();
+    }
+
+    public ArrayList<Zmienna> zmienne() {
+        return this.zmienne;
+    }
+    public boolean czyNalezy(Zmienna z){
+        if(zmienne.contains(z)) return true;
+        return false;
+    }
+    public boolean czyNalezy(char z){
+        for (Zmienna i: this.zmienne) {
+            if(i.nazwa()==z) return true;
+        }
+        return false;
+    }
+    public void dodaj(Zmienna z){
+        for (Zmienna i: this.zmienne) {
+            if(i.nazwa()==z.nazwa()) {
+                zmienne.remove(i);
+                break;
+            }
+        }
+        zmienne.add(new Zmienna(z.nazwa(), z.wartosc()));
+    }
+    public void dodaj(char z){
+        for (Zmienna i: this.zmienne) {
+            if(i.nazwa()==z) {
+                zmienne.remove(i);
+                break;
+            }
+        }
+        zmienne.add(new Zmienna(z));
+    }
+    public void dodaj(char z, int wartosc){
+        for (Zmienna i: this.zmienne) {
+            if(i.nazwa()==z) {
+                zmienne.remove(i);
+                break;
+            }
+        }
+        zmienne.add(new Zmienna(z, wartosc));
+    }
+    public void zmienWartosc(Zmienna z, int w){
+        for (Zmienna i: this.zmienne) {
+            if(i.nazwa()==z.nazwa()) {
+                i.zmienWartosc(w);
+                return;
+            }
+        }
+    }
+    public void zmienWartosc(char z, int w){
+        for (Zmienna i: this.zmienne) {
+            if(i.nazwa()==z) {
+                i.zmienWartosc(w);
+                return;
+            }
+        }
+    }
+    public void rzutuj(Zmienne b){
+        this.zmienne.clear();
+        this.zmienne.addAll(b.zmienne());
+    }
+
+}
